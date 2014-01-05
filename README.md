62ac21e6bbf358295d75e483e9fef94ae2a8146b
ARAKI Yasuhiro <ar@debian.org>
2014-01-06 08:00:42 +0900

    add README.md

--- README.md
+++ README.md
@@ -1,4 +1,75 @@
-node-s3-multisync-auto
-======================
+node-s3-multisync-auto (node-s3ma)
+============================================
 
 Automatic local file backup to two of S3 buckets.
+
+Your local PC directry (A) -> AWS S3 bucket (B) -> AWS S3 bucket (C)
+
+1. node-s3-multisync-auto (node-s3ma) detects file modification under running directory (A).
+2. node-s3ma uploads modified file form (A) to the AWS S3 bucket (B).
+3. node-s3ma syncs uploaded file from (B) bucket to (C) bucket.
+
+Setup
+---------------
+## Install
+
+```
+npm install cluster node-watch aws-sdk os mime fs
+```
+
+## Config
+
+Please use config.json and config_mime.types.
+
+### config.json
+
+```
+{
+    "accessKeyId": "AKIA******",
+    "secretAccessKey": "**********",
+    "region": "us-west-2",
+    "bucket": "*******-oregon", 
+	
+    "topPrefix": "backup/",
+    "bucketSync": "**********-ireland",
+    "topPrefixSync": "backup/",
+    "endpointSync": "https://s3-eu-west-1.amazonaws.com"
+}
+```
+
+* "accessKeyId": KeyId for S3.
+* "secretAccessKey": Key for S3.
+
+* "region": Bucket region of the 1st upload.
+* "bucket": Bucket name of the 1st upload.
+* "topPrefix": Bucket key prefix of the 1st upload.
+
+* "bucketSync": Bucket name of the 2nd sync.
+* "topPrefixSync": Bucket key prefix of the 2nd sync.
+* "endpointSync": Bucket endpoint URL of the 2nd sync.
+
+### config_mime.types
+
+```
+application/x-test  test
+```
+
+It is come from https://npmjs.org/package/mime.
+
+Use
+---------
+
+```
+cd /home/account/target
+node node-s3ma.js
+```
+
+TODO
+---------
+
+* Daemonize with forever (https://github.com/nodejitsu/forever).
+* Multipart S3 uplord for large file.
+
+
+
+
---
+++ node-s3ma.js
@@ -0,0 +1,84 @@
+var cluster = require('cluster');
+var watch = require('node-watch');
+var AWS = require('aws-sdk');
+var numCPUs = require('os').cpus().length;
+var mime = require('mime');
+var fs = require('fs');
+
+var numReqs = 0;
+var worker;
+var config = require('./config.json');
+
+AWS.config.loadFromPath('./config.json');
+var s3 = new AWS.S3();
+mime.load('./config_mime.types');
+
+var s3sync = new AWS.S3({endpoint: config.endpointSync});
+
+
+if (cluster.isMaster) {
+    for (var i = 0; i < numCPUs; i++) {
+	worker = cluster.fork();
+
+	watch('.', function(filename) {
+	    worker.send({chat: filename+' is changed.', watchfile: filename});  
+	});
+    }
+} else {
+    process.on('message', function(msg) {
+	if (msg.watchfile) {
+	    uploadingFile(msg.watchfile);
+	};
+    });
+}
+
+
+function uploadingFile(filename) {
+    fs.readFile(filename, function(err, data) {
+	if (err) {console.log(err)}
+	else {	    
+	    if (data.length == 0)
+		console.log("No upload. Because filesize = "+ data.length);
+	    else
+		uploading(filename, data);
+	}
+    })
+};
+
+
+function uploading(filename, something) {
+    var params = {
+	Bucket: config.bucket,
+	Key: config.topPrefix + filename,
+	Body: something,
+	ContentType: mime.lookup(filename)
+    };
+
+    s3.putObject(params, function(err, data) {
+	if (err){
+            console.log("**** " + filename + " ****");
+	    console.log(err);
+	   }
+	else {
+	    console.log("Successfully uploaded data to " + config.bucket+'/'+params.Key+ ". ETag is " + data.ETag);
+	    syncfile(filename);
+	}
+    });
+};
+
+function syncfile(filename){
+    var params = {
+	Bucket: config.bucketSync,
+	Key: config.topPrefixSync + filename,
+	CopySource: encodeURIComponent(config.bucket+'/'+config.topPrefix+filename)
+    };
+
+    s3sync.copyObject(params, function(err,data){
+	if (err)
+            console.log(err)
+	else {
+	    console.log("Successfully synced from " + decodeURIComponent(params.CopySource) + " to " + params.Bucket + "/" + params.Key + ". LastModified is " + data.LastModified);
+	}
+    });
+    
+};
